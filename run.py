c9d16a4aa361a6d49128c732c7484444500c5f37
Umang <umangaggarwal537@gmail.com>
2021-03-13 13:34:35 +0530

    updating templates

--- run.py
+++ run.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+import pickle
 
-@app.route('/')
+file = open('model.pkl', 'rb')
+clf = pickle.load(file)
+file.close()
+
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    return 'Hello, World!'
+    if request.method == "POST":
+        myDict = request.form
+        fever = int(myDict['fever'])
+        age = int(myDict['age'])
+        pain = int(myDict['pain'])
+        runnyNose = int(myDict['runnyNose'])
+        diffBreath = int(myDict['diffBreath'])
+
+        
+        inputFeatures = [fever, pain, age, runnyNose, diffBreath]
+        infProb = clf.predict_proba([inputFeatures])[0][1]
+        print(infProb)
+
+        
+        if(infProb >= 50):
+            return render_template('showabove.html', inf=round(infProb*100))
+        else:
+            return render_template('showbelow.html', inf=round(infProb*100))
+
+  
+    return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
